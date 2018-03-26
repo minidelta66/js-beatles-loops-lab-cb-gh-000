333973a3273ecfccd38edefb4e874efdac8b2b2d
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:04:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++ ) {
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+var array = [];
+var co = 0;
+while(co < facts.length) {
+  array.push(facts[co] + '!!!');
+  co++;
+}
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+    number++;
+  } while (number < 15);
+  return array;
+}
